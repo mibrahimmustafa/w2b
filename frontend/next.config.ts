e14b3aef449023e3d4198699654fb77659dc2434
mibrahimmustafa <m.ibrhaim.mustafa@gmail.com>
2026-04-25 21:00:09 +0300

    fix an issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // @ts-ignore - Next.js internal dev option
-  allowedDevOrigins: ["192.168.56.1", "localhost:3000"],
+  allowedDevOrigins: ["46.202.155.132", "192.168.56.1", "localhost:3000"],
   // To silence the Turbopack root warning
   serverExternalPackages: [], 
 };
